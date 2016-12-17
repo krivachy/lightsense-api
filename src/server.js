93884e8714582c3861192cd1fce3be677149a614
Akos Krivachy <akos.krivachy@futurice.com>
2016-12-17 17:43:44 +0100

    Move message creation outside

--- src/server.js
+++ src/server.js
@@ -17,6 +17,7 @@ wss.broadcast = function broadcast(data) {
 };
 
 const minutes = 5;
+const triggerOnMessage = 'trigger_on|' + minutes;
 
 wss.on('connection', function connection(ws) {
   const id = uuid();
@@ -28,8 +29,7 @@ wss.on('connection', function connection(ws) {
     if (data == 'turn_on') {
       wss.clients.forEach(function each(client) {
         if (client !== ws) {
-          const message = 'trigger_on|' + minutes;
-          console.log('Sending to client ' + client.clientId, message);
+          console.log('Sending to client ' + client.clientId, triggerOnMessage);
           client.send(message);
         }
       });
