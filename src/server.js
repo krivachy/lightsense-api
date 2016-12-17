90d143506e00b8b7241e4d9a68d70e60767f90f9
Akos Krivachy <akos.krivachy@futurice.com>
2016-12-17 22:52:08 +0100

    Make things configurable

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
+// Config
 const port = process.env.PORT || 3000;
+const pingInterval = process.env.PING_INTERVAL || 45000;
+const minutes = process.env.TRIGGER_ON_MINUTES || 5;
 const secretToken = process.env.SECRET_TOKEN || 'secret';
 const secretPath = '/' + secretToken;
+
 const wssOptions = {
   port: port,
+  // So that `ws.clients` will be set and tracked:
   clientTracking: true,
   // Use the path as "authentication", only allow clients to connect this path
   path: secretPath,
@@ -19,9 +24,6 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
-
-const minutes = 5;
-const triggerOnMessage = 'trigger_on|' + minutes;
 
 wss.on('connection', function connection(ws) {
   const id = uuid();
@@ -40,6 +42,7 @@ wss.on('connection', function connection(ws) {
     if (data == 'turn_on') {
       wss.clients.forEach(function each(client) {
         if (client !== ws) {
+          const triggerOnMessage = 'trigger_on|' + minutes;
           console.log('Sending to client ' + client.clientId, triggerOnMessage);
           client.send(triggerOnMessage);
         }
@@ -59,8 +62,9 @@ function pingClients(callback) {
   callback();
 }
 
+// We need to ping our clients at least every 55 seconds or Heroku closes our connection
 function scheduleClientPing() {
-  setTimeout(pingClients, 45000, scheduleClientPing);
+  setTimeout(pingClients, pingInterval, scheduleClientPing);
 }
 
 wss.on('listening', function () {
